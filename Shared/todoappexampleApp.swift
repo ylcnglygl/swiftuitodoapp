4a8024506609d887d4a8829c960c5a16e934ce9a
yalcin.golayoglu <yalcin.golayoglu@niltekyazilim.com>
2021-08-30 16:31:32 +0300

    Initial Commit

---
+++ Shared/todoappexampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappexampleApp.swift
+//  Shared
+//
+//  Created by Yalçın Golayoğlu on 30.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct todoappexampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
